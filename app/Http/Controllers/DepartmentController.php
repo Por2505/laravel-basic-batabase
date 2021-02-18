07f05087aeba4726b8d8b91963e8dd485ca3e88f
Por2505 <s6035512061@phuket.psu.ac.th>
2021-02-18 17:44:46 +0700

    บันทึกข้อมูลquery builder

--- app/Http/Controllers/DepartmentController.php
+++ app/Http/Controllers/DepartmentController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Department;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 class DepartmentController extends Controller
 {
@@ -23,12 +24,18 @@ class DepartmentController extends Controller
                 'department_name.unique'=>"มีข้อมูลชื่อแผนกนี้ในฐานข้อมูลแล้ว"
             ]
         );
-        $department = new Department;
-        $department->department_name = $request->department_name;
-        $department->user_id = Auth::user()->id;
-        $department->save();
-        return redirect()->back()->with('success',"บันทึกข้อมูลเรียบร้อยแล้ว");
+        //eloquent
+        // $department = new Department;
+        // $department->department_name = $request->department_name;
+        // $department->user_id = Auth::user()->id;
+        // $department->save();
 
+        //query builder
+        $data = array();
+        $data["department_name"]= $request->department_name;
+        $data["user_id"]= Auth::user()->id;
+        DB::table('departments')->insert($data);
+        return redirect()->back()->with('success',"บันทึกข้อมูลเรียบร้อยแล้ว");
 
     }
 }
